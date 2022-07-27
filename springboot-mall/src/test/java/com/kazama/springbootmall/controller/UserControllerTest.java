42daf8c2af8571f0c3f9cb3e857711f62d030640
Kazama1996 <Y.Kazama1996@gmail.com>
2022-07-27 14:55:51 +0800

    帳號功能：實作UserController.java單元測試

--- springboot-mall/src/test/java/com/kazama/springbootmall/controller/UserControllerTest.java
+++ springboot-mall/src/test/java/com/kazama/springbootmall/controller/UserControllerTest.java
@@ -3,6 +3,7 @@ package com.kazama.springbootmall.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kazama.springbootmall.dao.UserDao;
+import com.kazama.springbootmall.dto.UserLoginRequest;
 import com.kazama.springbootmall.dto.UserRegisterRequest;
 import com.kazama.springbootmall.model.User;
 import org.junit.jupiter.api.Test;
@@ -56,7 +57,158 @@ class UserControllerTest {
         // 檢查資料庫中的密碼不為明碼
         User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
         assertNotEquals(userRegisterRequest.getPassword(), user.getPassword());
+    }
+    @Test
+    public void register_InvalidEmail() throws Exception {
+        UserRegisterRequest userRegisterRequest = new UserRegisterRequest();
+        userRegisterRequest.setEmail("12123123");
+        userRegisterRequest.setEmail("123");
 
+        String json = objectMapper.writeValueAsString(userRegisterRequest);
+
+        RequestBuilder requestBuilder=MockMvcRequestBuilders.post("/user/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder).andExpect(status().is(400));
+    }
+
+    @Test
+    public void InvalidPassword() throws Exception {
+        UserRegisterRequest userRegisterRequest = new UserRegisterRequest();
+        userRegisterRequest.setEmail("12123123");
+        userRegisterRequest.setEmail("");
+
+        String json = objectMapper.writeValueAsString(userRegisterRequest);
+
+        RequestBuilder requestBuilder=MockMvcRequestBuilders.post("/user/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder).andExpect(status().is(400));
+
+    }
+
+    @Test
+    public void register_emailAlreadyExist() throws Exception {
+        UserRegisterRequest userRegisterRequest = new UserRegisterRequest();
+        userRegisterRequest.setEmail("asdasdadasda@gmail.com");
+        userRegisterRequest.setPassword("123asdasd456");
+
+        String json = objectMapper.writeValueAsString(userRegisterRequest);
+
+        RequestBuilder requestBuilder=MockMvcRequestBuilders.post("/user/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder).andExpect(status().is(201));
+
+
+        mockMvc.perform(requestBuilder).andExpect(status().is(400));
+
+    }
+
+
+    @Test
+    public void login_success() throws Exception {
+        UserRegisterRequest userRegisterRequest = new UserRegisterRequest();
+        userRegisterRequest.setEmail("adam@mail.com");
+        userRegisterRequest.setPassword("Asd850328");
+
+        register_Account(userRegisterRequest);
+
+
+        UserLoginRequest userLoginRequest = new UserLoginRequest();
+
+        userLoginRequest.setEmail(userRegisterRequest.getEmail());
+        userLoginRequest.setPassword(userLoginRequest.getPassword());
+
+        String json = objectMapper.writeValueAsString(userRegisterRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/user/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(200))
+                .andExpect(jsonPath("$.user_id", notNullValue()))
+                .andExpect(jsonPath("$.e_mail", equalTo(userRegisterRequest.getEmail())))
+                .andExpect(jsonPath("$.created_date", notNullValue()))
+                .andExpect(jsonPath("$.last_modified_date", notNullValue()));
+
+    }
+    @Test
+    public void login_invalidEmailFormat() throws Exception {
+        UserLoginRequest userLoginRequest = new UserLoginRequest();
+        userLoginRequest.setEmail("hkbudsr324");
+        userLoginRequest.setPassword("123");
+
+        String json = objectMapper.writeValueAsString(userLoginRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/user/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(400));
+    }
+
+    @Test
+    public void login_EmailNotFound() throws Exception {
+
+
+        UserLoginRequest userLoginRequest = new UserLoginRequest();
+
+        userLoginRequest.setEmail("asdasda@gmail.com");
+        userLoginRequest.setPassword("123123");
+
+        String json = objectMapper.writeValueAsString(userLoginRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/user/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(400));
+    }
+
+    @Test
+    public void login_InvalidPassword() throws Exception {
+        UserRegisterRequest userRegisterRequest = new UserRegisterRequest();
+        userRegisterRequest.setEmail("test123@mail.com");
+        userRegisterRequest.setPassword("Asd850328");
+
+        register_Account(userRegisterRequest);
+
+
+        UserLoginRequest userLoginRequest = new UserLoginRequest();
+
+        userLoginRequest.setEmail(userRegisterRequest.getEmail());
+        userLoginRequest.setPassword("1231231231231");
+
+        String json = objectMapper.writeValueAsString(userLoginRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/user/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(400));
+
+    }
+    
+    
+    private void register_Account(UserRegisterRequest userRegisterRequest) throws Exception {
+        
+            String json= objectMapper.writeValueAsString(userRegisterRequest);
+            RequestBuilder requestBuilder  = MockMvcRequestBuilders.post("/user/register")
+                            .contentType(MediaType.APPLICATION_JSON)
+                                    .content(json);
+            
+            mockMvc.perform(requestBuilder).andExpect(status().is(201));
+            
     }
 
 }
